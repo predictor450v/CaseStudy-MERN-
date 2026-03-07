cffc9e5722dcbf7b542bbb99b3b99e164204596d
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-03-07 22:20:01 +0530

    case study 1

---
+++ typescript_casestudy/cs1/casestudy1.js
@@ -0,0 +1,6 @@
+// first typescript file
+// let message: string = "Hello, World!";
+// console.log(message);
+// challenge
+var message = "ayushman";
+console.log("Hello, " + message);
---
+++ typescript_casestudy/cs1/casestudy1.ts
@@ -0,0 +1,6 @@
+// first typescript file
+// let message: string = "Hello, World!";
+// console.log(message);
+// challenge
+let message: string = "ayushman";
+console.log("Hello, " + message);
