01d802925b1739a1a43ec61047455ae75abc3529
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-03-07 22:18:40 +0530

    info

---
+++ typescript_casestudy/introfile.ts
@@ -0,0 +1,14 @@
+// install node js 
+// install typescript
+// npm install -g typescript
+
+// tsc --version
+
+// create a file with .ts extension
+
+// compile the typescript file to javascript using tsc command
+
+// tsc introfile.ts
+// run the compiled javascript file using node command
+// node introfile.js
+// ts-node hello.ts // to run typescript file without compiling it to javascript
